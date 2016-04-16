8c52d842ecd051d9cff4205abf9772f3235609f0
William Diedrichsen Marstrand <wdmdevelop@gmail.com>
2016-04-16 22:16:41 +0200

    Load button v. 0.1

---
+++ app/src/main/java/copenhacks/myapplication/ListFestivals.java
@@ -0,0 +1,26 @@
+package copenhacks.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by William Marstrand on 4/16/2016.
+ */
+public class ListFestivals extends AppCompatActivity {
+
+    private String[] festivals = new String[] {"Roskilde Festival", "Smuk Festival", "Grøn Koncert"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.festival_list);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.festival_list, festivals);
+
+        //ListView listView = (ListView) findViewById(R.id.festival_view);
+        //listView.setAdapter(adapter);
+    }
+
+}
--- app/src/main/java/copenhacks/myapplication/MainActivity.java
+++ app/src/main/java/copenhacks/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package copenhacks.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, ListFestivals.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
